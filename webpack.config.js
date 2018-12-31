b5fe78fa3145dac62cdcb3a55d631634b86c5f1d
vzteufem <marc.teufel@hama.de>
2018-12-31 05:19:31 +0100

    Inital checkin based on the fromscratch template repository

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.(js)$/, use: 'babel-loader' },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+        ]
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebPackPlugin( { template: 'src/index.html' } )
+    ]
+};
