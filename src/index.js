40ae9c764fa35ed1f92ad2615ee9e82f1c750ad9
vzteufem <marc.teufel@hama.de>
2019-01-01 08:10:13 +0100

    Implemented a simple dialog

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import Dialog from "./components/Dialog";
 
 class App extends React.Component {
+
+    state = {
+        isOpen: false
+    }
+
     render() {
         return(
-            <div>A Simple Dialog!!</div>
+            <div>
+
+                <button onClick={  (e) => this.setState( { isOpen: true } ) } >Open Dialog</button>
+
+
+                <Dialog isOpen={this.state.isOpen} onClose={(e) => this.setState( { isOpen: false } ) }>
+                    Ich bin ein menschlicher Mensch!!!
+                </Dialog>
+
+
+            </div>
         )
     }
 }
